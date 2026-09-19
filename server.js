20d4037708ed39bcd2945de74d1c334aaa2b846c
r4hkg4ft48-crypto <r4hkg4ft48@privaterelay.appleid.com>
2026-09-19 17:02:35 +0300

    Allow Telegram Authorization header for Mini App orders

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app=express();
 app.use(express.json());
 app.use((req,res,next)=>{
  res.setHeader('Access-Control-Allow-Origin','*');
- res.setHeader('Access-Control-Allow-Headers','Content-Type, X-Owner-Token');
+ res.setHeader('Access-Control-Allow-Headers','Content-Type, X-Owner-Token, Authorization');
  res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,OPTIONS');
  if(req.method==='OPTIONS') return res.sendStatus(204);
  next();
